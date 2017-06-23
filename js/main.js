79ec5c6c304d7a800dc8f47e77ebf523eebd5a64
Junnel Reboquio <jreboqui@ucsd.edu>
2017-06-22 21:41:16 -0700

    Added javascript and jQuery

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function main() {
+ 
+  $('.banner').hide();
+  $('#bannerP1').hide();
+  $('#bannerP2').hide();
+  $('#bannerP3').hide();
+  $('.banner').fadeIn(3000);
+  
+  $('#bannerP1').fadeIn(9000);
+  $('#bannerP2').fadeIn(8000);
+  $('#bannerP3').fadeIn(7000);
+
+}
+
+ $(document).ready(main);
