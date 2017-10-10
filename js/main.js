d0c346d38b4f52fdb4d9912c6cb8ec8b6e015a5c
Junnel Reboquio <jreboqui@ucsd.edu>
2017-10-09 23:44:54 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function main() {
  // $('#bannerP2').hide();
  // $('#bannerP3').hide();
   $('.banner').fadeIn(3000);
-  
+  $('#banner-content').fadeIn(8000);
   $('#bannerP1').fadeIn(9000);
   $('#bannerP2').fadeIn(8000);
  
